22944b7838e8c357820584997b9c0c8ade6d4470
David Perez <davidjared123@gmail.com>
2025-03-13 23:43:43 -0400

    adding site url config to github pages ms config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import alpinejs from "@astrojs/alpinejs";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://davidjared123.github.io',
+  base: 'landing-joel',
   integrations: [tailwind(), alpinejs()],
   i18n: {
     defaultLocale: "en",
