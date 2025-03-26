e7310a46306970bd14f4ce9e1224f29f29bc34a0
David Perez <davidjared123@gmail.com>
2025-03-26 10:20:32 -0400

    gitignore edited dist

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,14 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import alpinejs from "@astrojs/alpinejs";
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  adapter: netlify({
+    edgeMiddleware: true
+  }),
   site: 'https://davidjared123.github.io',
   base: 'landing-astro-joel',
   integrations: [tailwind(), alpinejs()],
