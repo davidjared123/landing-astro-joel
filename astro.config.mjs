8e4d34d03273ee6dc21341244282147bcb6c2525
David Perez <davidjared123@gmail.com>
2025-03-26 15:54:44 -0400

    Ultimas pruebas con git

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,11 +6,15 @@ import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
+  output: 'static', // SSG para forms estáticos
+  // output: 'server',
   adapter: netlify({
     edgeMiddleware: true
   }),
   site: 'https://davidjared123.github.io',
+  buildOptions: {
+    inlineStyles: false, // Necesario para que Netlify detecte el form
+  },
   integrations: [tailwind(), alpinejs()],
   i18n: {
     defaultLocale: "en",
@@ -20,3 +24,5 @@ export default defineConfig({
     },
   },
 });
+
+
